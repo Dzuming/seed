c040df6a8f30a649f83a125efde543d37adb8130
Dawid <Dawid>
2016-12-01 19:09:59 +0100

    Add webpack and tests to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+const jslint = require('gulp-jslint');
+const webpack = require('webpack-stream');
+const mocha = require('gulp-mocha');
 gulp.task('default', function() {
   // place code for your default task here
 });
@@ -12,8 +16,9 @@ gulp.task('styles', function() {
       stream: true
     }))
 });
-gulp.task('watch', ['browserSync', 'styles'], function(){
-gulp.watch('./client/Templates/*.scss', ['styles']); 
+gulp.task('watch', ['browserSync', 'styles', 'babel', 'jslint', 'webpack', 'test'], function(){
+gulp.watch('./client/Templates/*.scss', ['styles']);
+gulp.watch('./client/Scripts/*.js', ['babel', 'webpack']); 
 })
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -22,3 +27,34 @@ gulp.task('browserSync', function() {
     },
   })
 })
+gulp.task('babel', () => {
+    return gulp.src('./client/Scripts/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./client/dist/js'));
+});
+gulp.task('jslint', function () {
+    return gulp.src(['./client/Scripts/*.js'])
+            .pipe(jslint({ /* this object represents the JSLint directives being passed down */ }))
+            .pipe(jslint.reporter( 'default' ));
+});
+gulp.task('webpack', function() {
+  return gulp.src('./client/Scripts/*.js')
+    .pipe(webpack({
+      // entry: {
+      //   app: 'src/app.js',
+      //   test: 'test/test.js',
+      // },
+      output: {
+        filename: 'bundle.js',
+      },
+    }))
+    .pipe(gulp.dest('./client/dist/'));
+});
+gulp.task('test', () => 
+    gulp.src('./test', {read: true})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha())
+        .pipe(mocha({reporter: 'mocha-istanbul'}))
+);
